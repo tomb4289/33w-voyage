a1d432a9a2753e0529a3a5064c81e362a3327621
Tommy Bourgeois <tommyb4289@gmail.com>
2025-06-30 14:40:19 -0400

    ajout search et folders

--- single.php
+++ single.php
@@ -1,28 +1,29 @@
 <?php
 
 /**
- * le modèle index
+ * le modèle single.php
  * Représente le modèle par défaut
  */
 
 ?>
 
 <?php get_header() ?>
-<h1>trace seulement à retirer -------------- search.php -----------</h1>
+<h1>trace seulement à retirer -------------- single.php -----------</h1>
 <section class="populaire">
   <?php if (have_posts()) {
     while (have_posts()) {
       /* affiche l'image « mise en avant » miniature */
       the_post();
-      the_post_thumbnail('thumbnail');
+      the_post_thumbnail('large');
   ?>
       <h1><?php
           /* affiche le titre pricipal du « post » */
           the_title(); ?></h1>
 
   <?php
-      /* cette fontion permet d'afficher l'ensemble du contenu du post (article ou page)*/
+      /* cette fontion permet d'afficher l'ensemble du contenu (même les images) du post (article ou page)*/
       the_content();
+      edit_post_link();
     }
   } ?>
 </section>
