7fb596cd2a430ab523c8790fbb1c5688b2a2f890
Tommy Bourgeois <tommyb4289@gmail.com>
2025-08-22 09:19:25 -0400

    updated 404.php pour utiliser get_template_part

--- 404.php
+++ 404.php
@@ -4,22 +4,10 @@
  * The template for displaying 404 pages (Not Found)
  */
 
-?>
-
-<?php get_header() ?>
+get_header(); ?>
 
 <main id="primary" class="site-main not-found">
+    <?php get_template_part('gabarit/erreur-404'); ?>
+</main>
 
-    <section class="error-404 not-found">
-        <header class="page-header">
-            <h1 class="page-title">Oops! Cette page n'existe pas.</h1>
-        </header><div class="page-content">
-            <p>Il semble que rien n'ait été trouvé à cet emplacement. Peut-être essayez-vous une recherche ou retournez à la page d'accueil ?</p>
-
-            <?php get_search_form(); // Displays WordPress's default search form ?>
-
-            <p style="margin-top: 20px;">
-                <a href="<?php echo esc_url( home_url('/') ); ?>" class="button">Retour à la page d'accueil</a>
-            </p>
-
-            </div></section></main><?php get_footer(); ?>
+<?php get_footer(); ?>
